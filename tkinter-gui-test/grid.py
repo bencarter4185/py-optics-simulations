84d5498e020e6d6ef57dfcc6982744a26e7c2ca9
bencarter4185 <bencarter4185@gmail.com>
2020-08-07 15:52:51 +0100

    Added folder + testfiles for GUI dev

---
+++ tkinter-gui-test/grid.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+
+# Creating a label widget
+myLabel = Label(root, text = "Hello world!")
+# Shove label onto the screen
+myLabel.pack()
+
+# Run the 'main loop' to open application
+root.mainloop()
